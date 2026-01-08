c1c941e84ec038b28af0b28eba3ee3e7ff212294
grishabysh2806-sketch <grishabysh2806@gmail.com>
2026-01-09 01:34:17 +0300

    Add files via upload

--- src/components/RollingText.tsx
+++ src/components/RollingText.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 
 interface RollingTextProps {
   text: string;
-  as?: keyof JSX.IntrinsicElements;
+  as?: React.ElementType;
   className?: string;
   stagger?: number;
   duration?: number;
@@ -12,23 +12,27 @@ interface RollingTextProps {
 
 export const RollingText: React.FC<RollingTextProps> = ({ 
   text, 
-  as: TagName = 'div', 
+  as: Tag = 'div', 
   className = "",
   stagger = 0.02,
   duration = 0.6
 }) => {
   const [isMounted, setIsMounted] = useState(false);
-  const Tag = TagName as React.ElementType;
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-  if (!isMounted) return <Tag className={className}>{text}</Tag>;
+  if (!isMounted) {
+    const FallbackTag = Tag as any;
+    return <FallbackTag className={className}>{text}</FallbackTag>;
+  }
+
+  const ContainerTag = Tag as any;
 
   return (
-    <Tag className={`${className} flex flex-wrap`}>
-      {text.split('').map((char, i) => (
+    <ContainerTag className={`${className} flex flex-wrap`}>
+      {text.split('').map((char: string, i: number) => (
         <span
           key={i}
           className="inline-block animate-[rollIn_0.5s_ease-out_forwards]"
@@ -41,6 +45,6 @@ export const RollingText: React.FC<RollingTextProps> = ({
           {char}
         </span>
       ))}
-    </Tag>
+    </ContainerTag>
   );
 };
